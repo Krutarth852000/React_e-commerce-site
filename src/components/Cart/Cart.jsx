0ce03d5e908e06ac82af763f73313524ae131d2d
Krutarth Patel <krutarthpatel@Krutarths-MacBook-Air.local>
2021-05-01 22:36:36 -0400

    Link myCart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,14 +1,34 @@
-import React, { Component } from 'react'
-import Title from '../Title'
-import CartColumn from './cartColumn'
+import React, { Component } from 'react';
+import Title from '../Title';
+import CartColumn from './cartColumn';
+import {ProductConsumer} from '../../contextx';
+import EmptyCart from './EmptyCart';
+import CartList from './CartList';
+import CartTotals from './CartTotals';
 
 export default class Cart extends Component {
     render() {
         return (
             <section>
+                <ProductConsumer>
+                    {value =>{
+                        const {cart} = value;
+                        if(cart.length>0){
+                            return(
+                                <React.Fragment>
+                                <Title name="your" title="cart"/>
+                                <CartColumn/>
+                                <CartList value={value}/>
+                                <CartTotals value={value}/>
+                                </React.Fragment>
+                            )
+                        }
+                        else{
+                            return <EmptyCart />
+                        }
+                    }}
                 
-                <Title name="your" title="cart"/>
-                <CartColumn/>
+                </ProductConsumer>
             </section>
         )
     }
--- src/components/Cart/EmptyCart.jsx
+++ src/components/Cart/EmptyCart.jsx
@@ -5,7 +5,7 @@ export default function EmptyCart() {
         <div className="container mt-5">
             <div className="row">
                 <div className="col-10 x-auto text-center text-title">
-                    <h1>Yyour caart is currently empty</h1>
+                    <h1>your cart is currently empty</h1>
                 </div>
             </div>
         </div>
